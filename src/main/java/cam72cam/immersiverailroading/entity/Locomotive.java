a3f5aeb89405673cc329560f92e7c0ab17b34d4c
Philipp <philippdal@gmail.com>
2025-05-07 10:01:45 +0200

    fix: normal force

--- src/main/java/cam72cam/immersiverailroading/entity/Locomotive.java
+++ src/main/java/cam72cam/immersiverailroading/entity/Locomotive.java
@@ -415,7 +415,7 @@ public abstract class Locomotive extends FreightTank{
 	/** Maximum force that can be between the wheels and the rails before it slips */
     protected final double getStaticTractiveEffort() {
         return getDefinition().getScriptedStartingTractionNewtons(gauge, this)
-                * Math.sin(Math.toRadians(getRotationPitch()))
+                * (1 + Math.sin(-Math.copySign(Math.toRadians(getRotationPitch()), getCurrentSpeed().metric())))
                 * Config.ConfigBalance.slopeMultiplier
                 * Config.ConfigBalance.tractionMultiplier
                 * (slipping ? 0.5 : 1);
