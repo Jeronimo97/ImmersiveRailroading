c4723d23b26f2ec0daf97d742070ec822d3a8609
Philipp <philippdal@gmail.com>
2025-11-09 16:27:13 +0100

    feat: add magnet brake readout
    MAGNETIC_BRAKE

--- src/main/java/cam72cam/immersiverailroading/gui/overlay/Readouts.java
+++ src/main/java/cam72cam/immersiverailroading/gui/overlay/Readouts.java
@@ -37,6 +37,7 @@ public enum Readouts {
     BRAKE_CYLINDER_PRESSURE,
     DYNAMIC_BRAKE,
     MAIN_AIR_RESERVOIR,
+    MAGNETIC_BRAKE,
     ;
 
     public float getValue(EntityRollingStock stock) {
@@ -124,6 +125,10 @@ public enum Readouts {
             case MAIN_AIR_RESERVOIR:
                 return (float) (stock instanceof Locomotive ?
                         ((Locomotive) stock).getMainAirReservoir() : 0);
+            case MAGNETIC_BRAKE:
+                return stock instanceof EntityMoveableRollingStock ?
+                        ((EntityMoveableRollingStock) stock).getMagnetBrakeNewton() > 0 ?
+                                1 : 0 : 0;
         }
         return 0;
     }
