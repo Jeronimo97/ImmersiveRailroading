26526575b72c6bf8ae5ea90abd2c942c8de0dccb
Philipp <philippdal@gmail.com>
2026-02-23 18:50:25 +0100

    feat: added curve resistance

--- src/main/java/cam72cam/immersiverailroading/registry/EntityRollingStockDefinition.java
+++ src/main/java/cam72cam/immersiverailroading/registry/EntityRollingStockDefinition.java
@@ -108,6 +108,7 @@ public abstract class EntityRollingStockDefinition {
     public double directFrictionCoefficient;
     private int magneticTrackBrake;
     private int speedBrakeSqueal;
+    private float rigidWheelbase;
     
     public SoundDefinition brakeHighSpeedSound;
     public SoundDefinition brakeLowSpeedSound;
@@ -497,6 +498,7 @@ public abstract class EntityRollingStockDefinition {
         // Locomotives default to linear brake control
         isLinearBrakeControl = properties.getValue("linear_brake_control").asBoolean();
         speedBrakeSqueal = properties.getValue("speed_brake_squeal").asInteger(45);
+        rigidWheelbase = properties.getValue("rigid_wheelbase").asFloat(2.5f);
 
         try {
             brakeMaterials = PhysicalMaterials.valueOf(properties.getValue("brake_shoe_material").asString(PhysicalMaterials.CAST_IRON.toString()));
@@ -963,5 +965,9 @@ public abstract class EntityRollingStockDefinition {
     
     public int getSpeedBrakeSqueal() {
         return speedBrakeSqueal;
+    }
+    
+    public float getRigidWheelbase() {
+        return rigidWheelbase;
     }
 }
